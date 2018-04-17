3f964c99377b01b1fa67e588223cff3907446ecb
ilayan19900722@gmail.com <liuyifei825601>
2018-04-17 12:03:46 +0800

    格式化代码，用4空格代替2空格

--- vertx-lang-jphp-codegen/src/main/java/io/vertx/codetrans/lang/jphp/JPhpLang.java
+++ vertx-lang-jphp-codegen/src/main/java/io/vertx/codetrans/lang/jphp/JPhpLang.java
@@ -10,33 +10,33 @@ import java.util.stream.Collectors;
 
 public class JPhpLang implements Lang {
 
-  @Override
-  public String id() {
-    return "jphp";
-  }
-
-  @Override
-  public CodeBuilder codeBuilder() {
-    return new JPhpCodeBuilder();
-  }
-
-  @Override
-  public Script loadScript(ClassLoader loader, String path, String method) throws Exception {
-    throw new UnsupportedOperationException("not yet implemented");
-  }
-
-  @Override
-  public File createSourceFile(File root, List<String> className, String methodName) {
-    File folder = new File(root, className.stream().collect(Collectors.joining(File.separator)));
-    if (methodName != null) {
-      folder = new File(folder, methodName);
+    @Override
+    public String id() {
+        return "php";
     }
-    return new File(folder.getParent(), folder.getName() + ".php");
-  }
 
-  @Override
-  public String getExtension() {
-    return "php";
-  }
+    @Override
+    public CodeBuilder codeBuilder() {
+        return new JPhpCodeBuilder();
+    }
+
+    @Override
+    public Script loadScript(ClassLoader loader, String path, String method) throws Exception {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    public File createSourceFile(File root, List<String> className, String methodName) {
+        File folder = new File(root, className.stream().collect(Collectors.joining(File.separator)));
+        if (methodName != null) {
+            folder = new File(folder, methodName);
+        }
+        return new File(folder.getParent(), folder.getName() + ".php");
+    }
+
+    @Override
+    public String getExtension() {
+        return "php";
+    }
 
 }
