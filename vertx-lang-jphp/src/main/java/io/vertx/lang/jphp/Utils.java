8950672beb313c819afeeedbccd8d38c835146f2
okou19900722 <ilayan19900722@gmail.com>
2018-05-30 10:45:59 +0800

    提供DataObject到DataObjectWrapper的方法

--- vertx-lang-jphp/src/main/java/io/vertx/lang/jphp/Utils.java
+++ vertx-lang-jphp/src/main/java/io/vertx/lang/jphp/Utils.java
@@ -209,16 +209,16 @@ public class Utils {
     /**
      * {@link ObjectMemory}<DATA_OBJECT>
      */
-    public static <D> boolean isDataObject(Environment env, Class<D> clazz, Function<JsonObject, D> function, Memory value) {
-        return DataObjectConverter.create(clazz, function).accept(env, value);
+    public static <D, B extends DataObjectWrapper<D>> boolean isDataObject(Environment env, Class<D> clazz, Function<JsonObject, D> function, Function2<Environment, D, B> creator, Memory value) {
+        return DataObjectConverter.create(clazz, function, creator).accept(env, value);
     }
 
-    public static <D> D convParamDataObject(Environment env, Class<D> clazz, Function<JsonObject, D> function, Memory value) {
-        return DataObjectConverter.create(clazz, function).convParam(env, value);
+    public static <D, B extends DataObjectWrapper<D>> D convParamDataObject(Environment env, Class<D> clazz, Function<JsonObject, D> function, Function2<Environment, D, B> creator, Memory value) {
+        return DataObjectConverter.create(clazz, function, creator).convParam(env, value);
     }
 
     public static <D, B extends DataObjectWrapper<D>> Memory convReturnDataObject(Environment env, Class<D> clazz, Function2<Environment, D, B> creator, Function<JsonObject, D> function, D value) {
-        return DataObjectConverter.<D, B>create(clazz, function).convReturnNotNull(env, creator, value);
+        return DataObjectConverter.<D, B>create(clazz, function, creator).convReturnNotNull(env, value);
     }
 
     /**
