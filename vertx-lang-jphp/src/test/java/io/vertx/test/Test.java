8c5c232f7b2270532b2726edb90e0583301e52fc
okou19900722 <ilayan19900722@gmail.com>
2018-07-19 10:04:47 +0800

    regen

---
+++ vertx-lang-jphp/src/test/java/io/vertx/test/Test.java
@@ -0,0 +1,11 @@
+package io.vertx.test;
+
+public class Test {
+    public static void main(String[] args) {
+        int i = 0b10011111111111111111111111111111;
+        System.out.println(i);
+        System.out.println(Integer.toBinaryString(i));
+        i = i >>> 10;
+        System.out.println(Integer.toBinaryString(i));
+    }
+}
