1e3fefd46d8f618027a758eaef5fcbde931c3bab
okou19900722 <ilayan19900722@gmail.com>
2018-10-23 11:04:31 +0800

    部分项目有package-info配置Module，但里面没有任何需要codegen执行的Class或者DataObject类，导致不会生成对应的Extension类而与services文件不匹配

--- vertx-lang-jphp-gen/src/main/java/io/vertx/lang/jphp/generator/ServiceLoaderGenerator.java
+++ vertx-lang-jphp-gen/src/main/java/io/vertx/lang/jphp/generator/ServiceLoaderGenerator.java
@@ -1,28 +1,26 @@
 package io.vertx.lang.jphp.generator;
 
-import io.vertx.codegen.ModuleModel;
+import io.vertx.codegen.Model;
 import io.vertx.codegen.writer.CodeWriter;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
-public class ServiceLoaderGenerator extends PhpGenerator<ModuleModel> {
+import java.util.*;
+
+public class ServiceLoaderGenerator extends PhpGenerator<Model> {
 
   public ServiceLoaderGenerator() {
-    this.kinds = Collections.singleton("module");
+    this.kinds = new HashSet<>(Arrays.asList("class", "dataObject"));
     this.incremental = true;
   }
 
   @Override
-  public String filename(ModuleModel model) {
+  public String filename(Model model) {
     return "resources/META-INF/services/php.runtime.ext.support.Extension";
   }
 
-  private Set<String> extensions = new HashSet<>();
+  private Set<String> extensions = new TreeSet<>();
 
   @Override
-  protected void render(ModuleModel model, int index, int size, Map<String, Object> session, CodeWriter writer) {
+  protected void render(Model model, int index, int size, Map<String, Object> session, CodeWriter writer) {
     extensions.add(PhpExtensionGenerator.fcq(model));
     if (index == size - 1) {
       for (String extension : extensions) {
